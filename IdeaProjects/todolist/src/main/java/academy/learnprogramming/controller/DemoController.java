5ef18f42e85e9866ef2cedf20a99e972e7585bc3
Kenneth Lu Diaz <ludiazken@gmail.com>
2021-01-31 09:36:25 -0500

    Adds controller

--- IdeaProjects/todolist/src/main/java/academy/learnprogramming/controller/DemoController.java
+++ IdeaProjects/todolist/src/main/java/academy/learnprogramming/controller/DemoController.java
@@ -1,10 +1,14 @@
 package academy.learnprogramming.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@Slf4j
 public class DemoController {
 
     // Automatically adds response to body
@@ -12,5 +16,19 @@ public class DemoController {
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping("welcome")
+    public String welcome(Model model) {
+        model.addAttribute("user", "Tim");
+        log.info("model = {}", model);
+
+        return "welcome";
+    }
+
+    @ModelAttribute("welcomeMessage")
+    public String welcomeMessage() {
+        log.info("welcomeMessage() called");
+        return "Welcome to this Demo Application.";
     }
 }
